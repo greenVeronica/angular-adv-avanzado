2aec8e58e00faf0c641351698e3c2e03432dc785
Veronica <green.veronica@gmail.com>
2020-08-17 17:01:37 -0300

    primer commit - diseño

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,32 @@
 import { NgModule } from '@angular/core';
+
 import { Routes, RouterModule } from '@angular/router';
+import { DashboardComponent } from './pages/dashboard/dashboard.component';
+import { LoginComponent } from './auth/login/login.component';
+import { RegisterComponent } from './auth/register/register.component';
+import { ProgressComponent } from './pages/progress/progress.component';
+import { Grafica1Component } from './pages/grafica1/grafica1.component';
+import { NopagefoundComponent } from './pages/nopagefound/nopagefound.component';
+import { PagesComponent } from './pages/pages.component';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: '' , component: PagesComponent,
+   children: [ // rutas hijas
+    {path: 'dashboard', component: DashboardComponent},
+    {path: 'grafica1', component: Grafica1Component},
+    {path: 'progress', component: ProgressComponent},
+     {path: '', redirectTo: '/dashboard', pathMatch: 'full'}
+  ]
+   },
+ 
+
+  {path: 'login', component: LoginComponent},
+  {path: 'register', component: RegisterComponent},
+
+  {path: '**', component: NopagefoundComponent}// cualquier otra direccion va al componente
+
+  ];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
